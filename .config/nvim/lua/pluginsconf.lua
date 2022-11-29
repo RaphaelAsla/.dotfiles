32b72ad01bf23fe07d56844ad98f4a8e77fc6073
Reavey <verdosee@noreply.com>
2022-11-30 01:14:10 +0200

    removed nvim.tree

--- .config/nvim/lua/pluginsconf.lua
+++ .config/nvim/lua/pluginsconf.lua
@@ -1,4 +1,3 @@
 require('plugins.lsp')
 require('plugins.cmp')
 require('plugins.lualine')
-require('plugins.tree')
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -119,16 +119,6 @@ _G.packer_plugins = {
     path = "/home/reavey/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
-  ["nvim-tree.lua"] = {
-    loaded = true,
-    path = "/home/reavey/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
-    url = "https://github.com/kyazdani42/nvim-tree.lua"
-  },
-  ["nvim-web-devicons"] = {
-    loaded = true,
-    path = "/home/reavey/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
-    url = "https://github.com/kyazdani42/nvim-web-devicons"
-  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/reavey/.local/share/nvim/site/pack/packer/start/packer.nvim",
