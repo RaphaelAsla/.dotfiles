e69cfb26f0c76b36651730236336c11628919fe4
Reavey <verdosee@noreply.com>
2023-03-06 00:16:18 +0200

    new font

--- .config/st/config.def.h
+++ .config/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char* font = "JetBrains Mono NL:pixelsize=16:antialias=true:autohint=true";
+static char* font = "iosevka:pixelsize=18:antialias=true:autohint=true";
 static char* font2[] = {"JoyPixels"};
 static int borderpx = 2;
 
