515e711af52c8405f791959e9a452122617fa7b1
Reavey <verdosee@noreply.com>
2023-03-12 17:21:10 +0200

    .

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -55,8 +55,8 @@ require('lspconfig').lua_ls.setup {
 			workspace = {
 				-- Make the server aware of Neovim runtime files
 				library = {
-						[vim.fn.expand('$VIMRUNTIME/lua')] = true,
-						[vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
+					[vim.fn.expand('$VIMRUNTIME/lua')] = true,
+					[vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
 				}
 			}
 		}
