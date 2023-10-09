4716e72617c13a1d558a8d63bc2473ae17a021a1
Reavey <raphaelasla127@gmail.com>
2023-10-09 17:18:00 +0300

    font style to bold

--- .config/st/config.h
+++ .config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char* font = "Terminus:pixelsize=18:antialias=true:autohint=true";
+static char* font = "Terminus:pixelsize=18:antialias=true:autohint=true:style=bold";
 static char* font2[] = {"JoyPixels"};
 static int borderpx = 2;
 
