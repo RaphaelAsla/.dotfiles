7a736131534d296d51c4b4af78d8a5a4cd0422b0
Reavey <raphaelasla127@gmail.com>
2023-10-19 20:13:35 +0300

    remove bold from font

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -6,7 +6,7 @@ static const unsigned int snap = 0;     /* snap pixel */
 static const unsigned int gappx = 3;
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
-static const char* fonts[] = {"Iosveka:size=13:style=bold", "Font Awesome 5 Free", "JoyPixels:size=8"};
+static const char* fonts[] = {"Iosveka:size=13", "Font Awesome 5 Free", "JoyPixels:size=8"};
 static const char dmenufont[] = "Iosveka:size=13";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
--- .config/st/config.def.h
+++ .config/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char* font = "Iosevka:pixelsize=18:antialias=true:autohint=true:style=bold";
+static char* font = "Iosevka:pixelsize=18:antialias=true:autohint=true";
 static char* font2[] = {"JoyPixels", "Font Awesome 5 Free"};
 static int borderpx = 2;
 
