066d55c9af3fd1e08891180a580e92c7ff545e61
Reavey <raphaelasla127@gmail.com>
2024-08-31 01:11:02 +0300

    indent for installed treesitter parsers (mostly for php, python and html)

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,6 +16,10 @@ require 'nvim-treesitter.configs'.setup {
 	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
 	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
+	indent = {
+		enable = true,
+	},
+
 	highlight = {
 		enable = true,
 
