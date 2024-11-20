a76155078cf3ee6a16abd665d31fd9309b2d7bec
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2024-11-20 03:00:38 +0200

    Ultisnips and back to oxocarbon theme

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -65,14 +65,17 @@ require("lazy").setup({
 	"airblade/vim-rooter",
 	"nvim-treesitter/nvim-treesitter",
 	"tree-sitter/tree-sitter",
+	"SirVer/ultisnips",
+	"honza/vim-snippets",
 	{
-		--"nyoom-engineering/oxocarbon.nvim",
-		"blazkowolf/gruber-darker.nvim",
+		"nyoom-engineering/oxocarbon.nvim",
+		--"blazkowolf/gruber-darker.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme gruber-darker]])
+			vim.opt.background = "dark"
+			vim.cmd([[colorscheme oxocarbon]])
 		end,
 	}
 })
